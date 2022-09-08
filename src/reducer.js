45c20b01ddefb33d366218b0e1a37dc3f6bdaa21
olaide-hok <olaidekareem43@yahoo.com>
2022-09-08 09:23:48 +0100

    Setup decrease action type in reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -17,12 +17,29 @@ const reducer = (state, action) => {
                     amount: cartItem.amount + 1
                 }
             }
+            return cartItem
         })
         return {
             ...state,
             cart: tempCart
         }
     }
+    if(action.type === "DECREASE") {
+        let tempCart = state.cart.map((cartItem) => {
+            if(cartItem.id === action.payload){
+                return {
+                    ...cartItem, 
+                    amount: cartItem.amount - 1
+                }
+            }
+            return cartItem
+        }).filter((cartItem) => cartItem.amount !== 0)
+        return {
+            ...state,
+            cart: tempCart
+        }
+    }
+
     return state
 }
 
