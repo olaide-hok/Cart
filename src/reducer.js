183314a7ece5e34114e06fc141720a369bd7b560
olaide-hok <olaidekareem43@yahoo.com>
2022-09-08 08:42:57 +0100

    Setup remove item functionality.

--- src/reducer.js
+++ src/reducer.js
@@ -3,6 +3,12 @@ const reducer = (state, action) => {
     if(action.type === "CLEAR_CART") {
         return { ...state, cart: [] }
     }
+    if(action.type === "REMOVE_ITEM") {
+        return {
+            ...state,
+            cart: state.cart.filter((cartItem) => cartItem.id !== action.payload)
+        }
+    }
     return state
 }
 
